03224c10a20032e66b6498808f8f435ee67227ea
Clara Lecroisey <clalecroisey@gmail.com>
2020-12-29 23:01:11 +0100

    feat(JobList): create component JobList and add api call to fetch jobs

---
+++ src/components/JobList.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+const WTTJ_JOBS_API_URL =
+  "https://www.welcomekit.co/api/v1/embed?organization_reference=Pg4eV6k";
+
+function JobList() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    fetchJobs()
+      .then((data) => setJobs(data.jobs))
+      .catch(console.log);
+  }, []);
+
+  const fetchJobs = () => fetch(WTTJ_JOBS_API_URL).then((res) => res.json());
+
+  return (
+    <div className="JobList">
+      {jobs.map((job, index) => (
+        <div key={index}>{job.name}</div>
+      ))}
+    </div>
+  );
+}
+
+export default JobList;
